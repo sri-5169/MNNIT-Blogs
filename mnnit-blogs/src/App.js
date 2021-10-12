d408c62d66a5ec4e895b566282c7142b667601d7
Srinath Pandey <72240749+sri-5169@users.noreply.github.com>
2021-10-12 18:10:20 +0530

    Implemented the Routing, and designed the Header
    In order to go to different web pages for different requests the routing is necessary.

--- mnnit-blogs/src/App.js
+++ mnnit-blogs/src/App.js
@@ -1,43 +1,40 @@
-import './App.css';
-import Header from './components/Header';
-import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
-import Profile from './components/Profile/Profile';
-import Home from './components/Home';
-import AboutMnnit from './components/AboutMnnit';
-import About from './components/About';
-import Login from './components/Login';
+import "./App.css";
+import Header from "./components/Header";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile/Profile";
+import Home from "./components/home/Home";
+import AboutMnnit from "./components/AboutMnnit";
+import About from "./components/About";
+import Login from "./components/Login";
 function App() {
   return (
     <>
-
-    <Router>
-      <div className="App">
-   
-    <Switch>
-   
-    <Route path = '/about' >
-    <Header></Header>
-   <About ></About> 
-    </Route> 
-    <Route path = '/aboutMnnit' >
-    <Header></Header>
-    <AboutMnnit /> 
-    </Route>
-    <Route path = '/login' >
-    <Header></Header>
-    <Login /> 
-    </Route>
-    <Route path = '/profile' >
-    <Header></Header>
-    <Profile /> 
-    </Route> 
-    <Route path = '/'>
-    <Header></Header>
-      <Home></Home>
-    </Route>
-      </Switch>
-      </div>
-    </Router>
+      <Router>
+        <div className="App" >
+          <Switch>
+            <Route path="/about">
+            <Header></Header>
+              <About></About>
+            </Route>
+            <Route path="/aboutMnnit">
+            <Header></Header>
+              <AboutMnnit />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/profile">
+            <Header></Header>
+              <Profile />
+            </Route>
+            <Route path="/">
+            <Header></Header>
+              <Home></Home>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </>
   );
 }
--- mnnit-blogs/src/components/Header.js
+++ mnnit-blogs/src/components/Header.js
@@ -1,21 +1,38 @@
+import { Search } from "@material-ui/icons";
 import React from "react";
-import { Link } from "react-router-dom";
 import "./Header.css";
+import logo from "../images/logo192.png";
+import { Link } from "react-router-dom";
+
 function Header() {
   return (
-    <>
-      <div className="header">
-        <div className="header__logo"></div>
-        <div className="header__title"></div>
-        <div className="header__search">SearchBox</div>
-        <div className="header__nav">
-          
-        <Link to='login'><p className="header__option">Sign In</p></Link>
-        <Link to='aboutMnnit'><p className="header__option">About MNNIT</p></Link>
-        <Link to ='about'><p className="header__option">About Us</p></Link>
+    <nav className="header">
+      <div className="header__left">
+        <Link to="/">
+          <img src={logo} className="header__logo" alt="IMG" />
+        </Link>
+
+        <Link to="/" className="header__home">
+          <span>MNNIT BLOGS</span>
+        </Link>
+
+        <div className="header__search">
+          <Search className="header__searchIcon" />
+          <input type="text" placeholder="Search" />
         </div>
       </div>
-    </>
+      <div className="header__right">
+        <Link to="/login" className="nav__links">
+          <span>Sign In</span>
+        </Link>
+        <Link to="/aboutMnnit" className="nav__links">
+          <span>About MNNIT</span>
+        </Link>
+        <Link to="/about" className="nav__links">
+          <span>About Us</span>
+        </Link>
+      </div>
+    </nav>
   );
 }
 
